175c3cdc3c3e7dab88e392b160d570bf95776d7e
Nontokozo <nsdube10@gmail.com>
2025-01-28 16:17:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+function selectCity(event){
+    
+
+}
+
 let currentTime = moment().tz.guess().format("LLLL");
 let displayTime = document.querySelector("#capital-city");
+displayTime.addEventListener("click", selectCity)
