8dc8a499a3edb93b980574cc3ee7636aae556fb5
Nontokozo <nsdube10@gmail.com>
2025-01-29 13:59:33 +0200

    Update index.html and index.js formula

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 function selectCity(event){
- if( event.target.value.length > 0 ){
-    let currentTime = moment().tz(event.target.value).format("LLLL");
-    
-    alert(`It is ${currentTime} in ${event.target.value}`)
- }
+ if(event.target.value === "Perth"){
+let perthTime = moment().tz("Australia/Perth").format("LLLL");
+alert(`It is ${perthTime} in ${event.target.value}`)
+}
+
+if(event.target.value === "Jamaica"){
+    let jamaicaTime = moment().tz("America/Jamaica").format("LLLL");
+    alert(`It is ${jamaicaTime} in ${event.target.value}`);
+}
+if(event.target.value === "Nairobi"){
+    let nairobiTime = moment().tz("Africa/Nairobi").format("LLLL");
+    alert(`It is ${nairobiTime} in ${event.target.value}`)
+}
+
+ 
 
 }
 
 
 let displayTime = document.querySelector("#capital-city");
 displayTime.addEventListener("change", selectCity)
+
